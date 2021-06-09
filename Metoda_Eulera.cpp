692f61edadec36c229a5c15645397aaf60b682cf
KonradKrzempek <72752980+KonradKrzempek@users.noreply.github.com>
2021-06-10 01:28:02 +0200

    Update Metoda_Eulera.cpp

--- Metoda_Eulera.cpp
+++ Metoda_Eulera.cpp
@@ -13,10 +13,10 @@ void euler(float x0, float y, float h, float x)
     cout << "  wynosi  " << setprecision(5) << fixed << y << endl;
 
     while (x0 < x) {  
-    //pêtla wykonywana do osi¹gniêcia przez punkt pocz¹tkowy wartoœci koñca przedzia³u
+    //petla wykonywana do osiagniecia przez punkt poczatkowy wartosci konca przedzialu
 
         y = y + h * f(x0, y); //obliczanie y
-        x0 = x0 + h;          //zwiêkszenie wartoœci x0 o h 
+        x0 = x0 + h;          //zwiekszenie wartosci x0 o h 
         cout << "Rozwiazanie dla x = " << setprecision(1) << fixed<< x0;
         cout<<"  wynosi  " << setprecision(5) << fixed << y << endl;
     }
@@ -24,10 +24,10 @@ void euler(float x0, float y, float h, float x)
 
 int main()
 {
-    float x0 = 0;  //pocz¹tek przedzia³u
+    float x0 = 0;  //poczatek przedzialu
     float y0 = 12;
     float h = 0.3; //krok
-    float x = 1.5;   //koniec przedzia³u
+    float x = 1.5;   //koniec przedzialu
 
     euler(x0, y0, h, x);
 
